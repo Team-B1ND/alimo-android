e53f6bce80b1c7842ed099d2515db8ac31551708
8954sood <58807462+8954sood@users.noreply.github.com>
2024-01-04 15:01:54 +0900

    feat: Create Base Code

---
+++ app/src/main/java/com/b1nd/alimo/base/BaseActivity.kt
@@ -0,0 +1,54 @@
+package com.b1nd.alimo.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import com.b1nd.alimo.utiles.Event
+import com.b1nd.alimo.utiles.repeatOnStarted
+import kotlinx.coroutines.flow.collectLatest
+
+abstract class BaseActivity<T: ViewDataBinding, VM: BaseViewModel>(
+    @LayoutRes val layoutRes: Int
+) : Fragment() {
+
+    protected lateinit var mBinding: T
+    protected lateinit var mViewModel: VM
+
+    protected abstract val viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mViewModel = if (::mViewModel.isInitialized) mViewModel else viewModel
+        mBinding.lifecycleOwner = this@BaseActivity
+        initView()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    abstract fun initView()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::mBinding.isInitialized) mBinding.unbind()
+    }
+
+    protected fun bindingViewEvent(action: (event: Event) -> Unit) {
+        repeatOnStarted {
+            viewModel.eventFlow.collectLatest {
+                action(it)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/b1nd/alimo/utiles/Extension.kt
@@ -0,0 +1,29 @@
+package com.b1nd.alimo.utiles
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
+    lifecycleScope.launch {
+        lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
+    }
+}
+
+internal fun ViewModel.launchMain(action: () -> Unit) {
+    viewModelScope.launch(Dispatchers.Main) {
+        action()
+    }
+}
+
+internal fun ViewModel.launchIO(action: suspend () -> Unit) {
+    viewModelScope.launch(Dispatchers.IO) {
+        action()
+    }
+}
