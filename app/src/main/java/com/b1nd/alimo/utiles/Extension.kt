ab838536f3b66cc5edca17ee54f7d7135da19e83
8954sood <58807462+8954sood@users.noreply.github.com>
2024-01-15 18:46:05 +0900

    feat: Create Animation Show And Hide

--- app/src/main/java/com/b1nd/alimo/utiles/Extension.kt
+++ app/src/main/java/com/b1nd/alimo/utiles/Extension.kt
@@ -1,6 +1,9 @@
 package com.b1nd.alimo.utiles
 
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
@@ -44,4 +47,16 @@ internal fun ImageView.loadImage(url: String) {
         .load(url)
         .centerCrop()
         .into(this)
+}
+
+fun Fragment.startAnimationWithShow(view: View, id: Int) {
+    view.visibility = View.VISIBLE  //애니메이션 전에 뷰를 보이게 한다
+    view.startAnimation(AnimationUtils.loadAnimation(requireContext(), id)) //애니메이션 설정&시작
+}
+
+//사라지기
+fun Fragment.startAnimationWithHide(view: View, id: Int) {
+    val exitAnim = AnimationUtils.loadAnimation(requireContext(), id)    //애니메이션 설정
+    exitAnim.setAnimationListener(HideAnimListener(view))   //리스너를 통해 애니메이션이 끝나면 뷰를 감춘다
+    view.startAnimation(exitAnim) //애니메이션 시작
 }
