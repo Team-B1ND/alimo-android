c355707619c6c9c4375d2d5d257cd3d13170a75d
8954sood <58807462+8954sood@users.noreply.github.com>
2024-01-15 22:07:30 +0900

    build: Add Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
 
+    val nav_version = "2.5.3"
+    
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
@@ -63,11 +65,12 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // circle
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
+    // paging
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 
+    // glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
-    implementation("com.github.amarjain07:StickyScrollView:1.0.3")
 }
