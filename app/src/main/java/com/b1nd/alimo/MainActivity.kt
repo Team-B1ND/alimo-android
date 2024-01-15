6a5ccb992fae1d548df2f587fda19440026b882f
8954sood <58807462+8954sood@users.noreply.github.com>
2024-01-15 18:46:20 +0900

    feat: Create Bookmark Fragment

--- app/src/main/java/com/b1nd/alimo/MainActivity.kt
+++ app/src/main/java/com/b1nd/alimo/MainActivity.kt
@@ -3,7 +3,7 @@ package com.b1nd.alimo
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.b1nd.alimo.feature.detail.DetailFragment
+import com.b1nd.alimo.feature.bookmark.BookmarkFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<FrameLayout>(R.id.frameLayout)
         supportFragmentManager.beginTransaction()
-            .add(R.id.frameLayout, DetailFragment())
+            .add(R.id.frameLayout, BookmarkFragment())
             .commit()
     }
 }
