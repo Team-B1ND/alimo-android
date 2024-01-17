b0f9be0f7125a1a6a7ded4f6842d071c0d9d80ff
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-01-17 21:23:32 +0900

    feat: Create Parent Jogin
    학부모 회원가입 Fragment

---
+++ app/src/main/java/com/b1nd/alimo/onboarding/parent/join/second/ParentJoinSecondFragment.kt
@@ -0,0 +1,16 @@
+package com.b1nd.alimo.onboarding.parent.join.second
+
+import androidx.fragment.app.viewModels
+import com.b1nd.alimo.R
+import com.b1nd.alimo.base.BaseFragment
+import com.b1nd.alimo.databinding.FragmentParentJoinSecondBinding
+
+class ParentJoinSecondFragment:BaseFragment<FragmentParentJoinSecondBinding, ParentJoinSecondViewModel>(
+    R.layout.fragment_parent_join_second
+) {
+    override val viewModel: ParentJoinSecondViewModel by viewModels()
+
+    override fun initView() {
+//        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/b1nd/alimo/onboarding/parent/join/third/ParentJoinThirdViewModel.kt
@@ -0,0 +1,6 @@
+package com.b1nd.alimo.onboarding.parent.join.third
+
+import com.b1nd.alimo.base.BaseViewModel
+
+class ParentJoinThirdViewModel:BaseViewModel() {
+}
