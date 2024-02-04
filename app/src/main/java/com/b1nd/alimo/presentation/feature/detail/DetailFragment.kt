2db4cb4d5cd845e3d8f2e1318c2bc2d340a88732
8954sood <58807462+8954sood@users.noreply.github.com>
2024-02-05 00:29:15 +0900

    feat: Create File Download Function

--- app/src/main/java/com/b1nd/alimo/presentation/feature/detail/DetailFragment.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/detail/DetailFragment.kt
@@ -1,6 +1,11 @@
 package com.b1nd.alimo.presentation.feature.detail
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.util.Log
+import android.util.Patterns
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -12,6 +17,7 @@ import com.b1nd.alimo.presentation.MainActivity
 import com.b1nd.alimo.presentation.base.BaseFragment
 import com.b1nd.alimo.presentation.feature.detail.DetailViewModel.Companion.ON_CLICK_BACK
 import com.b1nd.alimo.presentation.feature.detail.DetailViewModel.Companion.ON_CLICK_SEND
+import com.b1nd.alimo.presentation.utiles.getTimeString
 import com.b1nd.alimo.presentation.utiles.onSuccessEvent
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
@@ -86,4 +92,40 @@ class DetailFragment: BaseFragment<FragmentDetailBinding, DetailViewModel>(R.lay
             "testcontent\n알빠노\n라고할뻔",
         null)
     }
+
+    private fun downloadFile(
+        url: String
+    ) {
+        if (!Patterns.WEB_URL.matcher(url).matches()) { // 웹 url인지 유효성 검사
+            return
+        }
+        val extension = url.split(".").last()
+        Log.d("TAG", "downloadFile: $extension")
+        val fileName = "${getString(R.string.app_name)}/${getTimeString(extension)}"
+
+        val mimeType = when(extension) {
+            "jpg", "png", "jpeg", "gif", "bmp", "webp" -> "image/*"
+            "plane", "html", "css", "javascript", "txt", "json" -> "text/*"
+            "midi", "mpeg", "wav", "mp3" -> "audio/*"
+            "webm", "mp4", "ogg", "avi", "mkv", "flv", "m4p", "m4v" -> "video/*"
+            else -> "application/${extension}"
+        }
+        Log.d("TAG", "downloadFile: $mimeType")
+
+
+        val downloadManager = DownloadManager.Request(Uri.parse(url))
+
+        downloadManager.setTitle(fileName)
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) // 알림 설정
+            .setMimeType(mimeType)
+            .setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS,
+                fileName
+            ) // 다운로드 완료 시 보여지는 이름
+
+        val manager = requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(downloadManager)
+    }
+
+
 }
