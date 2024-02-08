77a355bf2733b61137468570b2ad7406a4cc6878
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-02-08 22:11:35 +0900

    feat: Splash Screen  StatusBarColor
    처음화면 상태바 색깔

--- app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstFragment.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstFragment.kt
@@ -1,7 +1,11 @@
 package com.b1nd.alimo.presentation.feature.onboarding.first
 
 
+import android.os.Bundle
 import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.b1nd.alimo.R
@@ -15,5 +19,23 @@ class OnboardingFirstFragment:BaseFragment<FragmentOnboardingFirstBinding, Onboa
         Handler().postDelayed({
             findNavController().navigate(R.id.action_onboardingFirst_to_onboardingSecond)
         }, 2000)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        activity?.window?.statusBarColor = context?.getColor(R.color.Main500)?: 0
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        activity?.window?.statusBarColor = context?.getColor(R.color.white)?: 0
     }
 }
