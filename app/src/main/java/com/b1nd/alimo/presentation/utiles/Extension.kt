80f8867ee445dc60f95737c61512f8d07b75a628
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-02-28 21:54:40 +0900

    feat: Start MainActivity
    OnboardingActivity에서 MainActivity로 가는 기능

--- app/src/main/java/com/b1nd/alimo/presentation/utiles/Extension.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/utiles/Extension.kt
@@ -1,10 +1,12 @@
 package com.b1nd.alimo.presentation.utiles
 
 import android.content.Context
+import android.content.Intent
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -95,4 +97,11 @@ fun Fragment.startAnimationWithHide(view: View, id: Int) {
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun Fragment.startActivityWithFinishAll(activity: Class<*>) {
+    val intent = Intent(context!!.applicationContext, activity)
+    intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+    startActivity(intent)
+    this.requireActivity().finishAffinity()
 }
