1e8d54a87d17dc5216caf32a0c8524fb4da569f4
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-03-06 11:35:53 +0900

    chore: Token Check
    토큰 확인하는거 수정

--- app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstFragment.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstFragment.kt
@@ -3,6 +3,7 @@ package com.b1nd.alimo.presentation.feature.onboarding.first
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,10 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.b1nd.alimo.R
 import com.b1nd.alimo.databinding.FragmentOnboardingFirstBinding
+import com.b1nd.alimo.presentation.MainActivity
 import com.b1nd.alimo.presentation.base.BaseFragment
+import com.b1nd.alimo.presentation.utiles.collectStateFlow
+import com.b1nd.alimo.presentation.utiles.startActivityWithFinishAll
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,15 +22,20 @@ class OnboardingFirstFragment:BaseFragment<FragmentOnboardingFirstBinding, Onboa
     override val viewModel: OnboardingFirstViewModel by viewModels()
 
     override fun initView() {
-        Handler().postDelayed({
-            if (viewModel.tokenCheck()){
-                findNavController().navigate(R.id.action_onboardingFirst_to_onboardingSecond)
-            }else{
-                findNavController().navigate(R.id.action_onboardingFirst_to_onboardingSecond)
+        viewModel.tokenCheck()
 
-//                startActivityWithFinishAll(MainActivity::class.java)
+        Handler().postDelayed({
+            collectStateFlow(viewModel.tokenState) {
+                Log.d("TAG", "initView: $it")
+                if (it) {
+                    startActivityWithFinishAll(MainActivity::class.java)
+                }else{
+                    findNavController().navigate(R.id.action_onboardingFirst_to_onboardingSecond)
+                }
             }
         }, 2000)
+
+
     }
 
     override fun onCreateView(
@@ -40,6 +49,10 @@ class OnboardingFirstFragment:BaseFragment<FragmentOnboardingFirstBinding, Onboa
         return super.onCreateView(inflater, container, savedInstanceState)
 
     }
+
+
+
+
 
     override fun onStop() {
         super.onStop()
--- app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstViewModel.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/first/OnboardingFirstViewModel.kt
@@ -1,9 +1,12 @@
 package com.b1nd.alimo.presentation.feature.onboarding.first
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.b1nd.alimo.data.repository.TokenRepository
 import com.b1nd.alimo.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,18 +15,22 @@ class OnboardingFirstViewModel @Inject constructor(
     private val tokenRepository: TokenRepository
 ):BaseViewModel() {
 
-    var failed = false
+    private val _tokenState = MutableStateFlow(false)
+    val tokenState = _tokenState.asStateFlow()
 
-    fun tokenCheck(): Boolean {
+    fun tokenCheck(){
         viewModelScope.launch {
-            val token = tokenRepository.getToken().token
-            val refreshToken = tokenRepository.getToken().refreshToken
-            if (token == null && refreshToken == null) {
-                failed = true
-
+            kotlin.runCatching {
+                tokenRepository.getToken()
+            }.onSuccess {
+                _tokenState.value = it.token != null
+                Log.d("TAG", "tokenCheck: $it")
+                Log.d("TAG", "tokenCheck: ${tokenState.value}")
+            }.onFailure {
+                Log.d("TAG", "tokenCheck: $it")
             }
         }
-        return failed
+
     }
 
 }
