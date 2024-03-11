6d7e47ed70395e0a86ae5ec877b70783920279ef
8954sood <58807462+8954sood@users.noreply.github.com>
2024-03-11 14:51:43 +0900

    feat: Create Image Download Logic

--- app/src/main/java/com/b1nd/alimo/data/Env.kt
+++ app/src/main/java/com/b1nd/alimo/data/Env.kt
@@ -1,5 +1,5 @@
 package com.b1nd.alimo.data
 
 object Env {
-    const val testToken = "eyJKV1QiOiJBQ0NFU1MiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiQXV0aG9yaXphdGlvbiI6IlRFQUNIRVIiLCJpYXQiOjE3MTAxMzMzMDMsImV4cCI6MTcxMDEzNTEwM30.HF6FN57MH_rXglEx616cyKiZld4kM_O49xDeMnBAPSqf52d49uFSc5pWGVV_zI3MgtxG8HFveyWzyZP0nxCqPA"
+    const val testToken = "eyJKV1QiOiJBQ0NFU1MiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiQXV0aG9yaXphdGlvbiI6IlRFQUNIRVIiLCJpYXQiOjE3MTAxMzU5MzQsImV4cCI6MTcxMDEzNzczNH0._89GOfWMWVyrVpiuwT1bcy5IM5nidnVe2o__3DV7JCOz8lQHHBKdi438ROvywlf4mORs_JfBvqbisEKT43wZXg"
 }
--- app/src/main/java/com/b1nd/alimo/presentation/utiles/Extension.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/utiles/Extension.kt
@@ -1,11 +1,15 @@
 package com.b1nd.alimo.presentation.utiles
 
+import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Build
+import android.os.Environment
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.view.WindowInsetsController
 import android.view.animation.AnimationUtils
@@ -201,5 +205,66 @@ fun Fragment.systemBarDark(isDark: Boolean) {
     } else {
         @Suppress("DEPRECATION")
         activity?.window?.decorView?.systemUiVisibility = if(isDark) 0 else View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+}
+
+fun Fragment.downloadFile(
+    url: String,
+    name: String,
+    extension: String,
+) {
+    try {
+        if (!Patterns.WEB_URL.matcher(url).matches()) { // 웹 url인지 유효성 검사
+            return
+        }
+//        val extension = url.split(".").last()
+        Log.d("TAG", "downloadFile: $extension")
+//        val fileName = "${getString(R.string.app_name)}/${getTimeString(extension)}"
+//        val fileName = "${getString(R.string.app_name)}/${name}" // "${getString(R.string.app_name)}/${name}"
+
+        val mimeType = when (extension.replace("application/", "")) {
+            "jpg", "png", "jpeg", "gif", "bmp", "webp" -> "image/*"
+            "plane", "html", "css", "javascript", "txt", "json", "csv" -> "text/*"
+            "midi", "mpeg", "wav", "mp3" -> "audio/*"
+            "webm", "mp4", "ogg", "avi", "mkv", "flv", "m4p", "m4v" -> "video/*"
+            else -> "application/${extension}"
+        }
+        Log.d("TAG", "downloadFile: $mimeType")
+
+
+        val downloadManager = DownloadManager.Request(Uri.parse(url))
+        val filePath = "/${getString(R.string.app_name)}/${name}"
+        downloadManager
+            .setTitle(filePath)
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED) // 알림 설정
+            .setMimeType(mimeType)
+//                .setDestinationInExternalFilesDir(
+//                    requireContext(),
+//                    Environment.DIRECTORY_DOWNLOADS,
+//                    "/${getString(R.string.app_name)}/${name}"
+//                )
+//                .setDestinationUri(Uri.parse(filePath))
+            // 작동하는데, download에도 같이 다운로드됨
+            .setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS,
+                filePath
+            ) // 다운로드 완료 시 보여지는 이름
+
+
+        val manager =
+            requireContext().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(downloadManager)
+    } catch (e: Exception) {
+        e.printStackTrace()
+//            if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+//                    requireContext().shortToast("다운로드를 위해 권환이 필요합니다.")
+//                    ActivityCompat.requestPermissions(requireActivity(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE, 1004);
+//                }
+//                else {
+//                    Toast.makeText(getBaseContext(), "다운로드를 위해\n권한이 필요합니다.", Toast.LENGTH_LONG).show();
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1004);
+//                }
+//            }
     }
 }
