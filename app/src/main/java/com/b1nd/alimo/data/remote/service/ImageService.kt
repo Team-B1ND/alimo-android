65b07425c9c054007ae289992a135da25059e490
8954sood <58807462+8954sood@users.noreply.github.com>
2024-03-13 08:42:30 +0900

    feat: Create Image Remote Logic

--- app/src/main/java/com/b1nd/alimo/data/remote/service/ImageService.kt
+++ app/src/main/java/com/b1nd/alimo/data/remote/service/ImageService.kt
@@ -1,10 +1,14 @@
 package com.b1nd.alimo.data.remote.service
 
+import com.b1nd.alimo.data.Env.testToken
 import com.b1nd.alimo.data.remote.response.BaseResponse
 import com.b1nd.alimo.data.remote.response.Image.ImageResponse
-import com.b1nd.alimo.data.remote.response.notification.FileResponse
 import io.ktor.client.HttpClient
-import java.time.LocalDateTime
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.headers
+import io.ktor.client.request.parameter
 import javax.inject.Inject
 
 class ImageService @Inject constructor(
@@ -13,34 +17,39 @@ class ImageService @Inject constructor(
 
     suspend fun getNotificationImage(
         notificationId: Int
-    ): BaseResponse<ImageResponse> = dummyNotificationImage()
+    ): BaseResponse<ImageResponse> = httpClient.get("/image/get") {
+        headers {
+            header("Authorization", "Bearer $testToken")
+        }
+        parameter("notificationId", notificationId)
+    }.body()
 
-    private fun dummyNotificationImage() =
-        BaseResponse(
-            status = 200,
-            message = "",
-            data = ImageResponse(
-                0,
-                0,
-                "test",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvc9_EjDkhzN51pg9u9N6X56tWgB5BCfxRZ3oE-_jwzw&s",
-                LocalDateTime.now(),
-                listOf(
-                    FileResponse(
-                        "https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp",
-                        "iuImage.png",
-                        0,
-                        "",
-                        ""
-                    ),
-                    FileResponse(
-                        "https://www.nme.com/wp-content/uploads/2021/02/iu-lilac-photo-edam-entertainment@2000x1270.jpg",
-                        "iuImage.jpg",
-                        0,
-                        "",
-                        ""
-                    ),
-                )
-            )
-        )
+//    private fun dummyNotificationImage() =
+//        BaseResponse(
+//            status = 200,
+//            message = "",
+//            data = ImageResponse(
+//                0,
+//                0,
+//                "test",
+//                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvc9_EjDkhzN51pg9u9N6X56tWgB5BCfxRZ3oE-_jwzw&s",
+//                LocalDateTime.now(),
+//                listOf(
+//                    FileResponse(
+//                        "https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp",
+//                        "iuImage.png",
+//                        0,
+//                        "",
+//                        ""
+//                    ),
+//                    FileResponse(
+//                        "https://www.nme.com/wp-content/uploads/2021/02/iu-lilac-photo-edam-entertainment@2000x1270.jpg",
+//                        "iuImage.jpg",
+//                        0,
+//                        "",
+//                        ""
+//                    ),
+//                )
+//            )
+//        )
 }
