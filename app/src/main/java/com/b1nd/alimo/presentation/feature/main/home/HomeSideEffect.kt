679187d7cf28252face3f26e43ebd86baaa34be5
8954sood <58807462+8954sood@users.noreply.github.com>
2024-03-16 23:02:09 +0900

    refactor: Edit Home SideEffect Object To Data Object

--- app/src/main/java/com/b1nd/alimo/presentation/feature/main/home/HomeSideEffect.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/main/home/HomeSideEffect.kt
@@ -3,12 +3,12 @@ package com.b1nd.alimo.presentation.feature.main.home
 sealed class HomeSideEffect {
     data class NotFound(val found: HomeFound): HomeSideEffect()
     data class NetworkError(val message: String): HomeSideEffect()
-    object FailedChangeEmoji: HomeSideEffect()
-    object FailedChangeBookmark: HomeSideEffect()
+    data object FailedChangeEmoji: HomeSideEffect()
+    data object FailedChangeBookmark: HomeSideEffect()
 }
 
 sealed class HomeFound {
-    object Speaker: HomeFound()
-    object Category: HomeFound()
-    object Post: HomeFound()
+    data object Speaker: HomeFound()
+    data object Category: HomeFound()
+    data object Post: HomeFound()
 }
