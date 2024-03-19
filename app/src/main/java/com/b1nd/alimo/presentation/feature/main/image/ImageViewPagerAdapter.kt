26102c780ce8956106e3d3962fbd81db9be09883
8954sood <58807462+8954sood@users.noreply.github.com>
2024-03-19 20:05:52 +0900

    feat: Create Image Load Background Set White

--- app/src/main/java/com/b1nd/alimo/presentation/feature/main/image/ImageViewPagerAdapter.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/main/image/ImageViewPagerAdapter.kt
@@ -1,11 +1,14 @@
 package com.b1nd.alimo.presentation.feature.main.image
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.b1nd.alimo.data.model.FileModel
 import com.b1nd.alimo.databinding.ItemImageBinding
-import com.b1nd.alimo.presentation.utiles.loadNotCropImage
+import com.b1nd.alimo.presentation.utiles.BackgroundColorTransform
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 class ImageViewPagerAdapter(
     private val imageList: List<FileModel>,
@@ -20,7 +23,13 @@ class ImageViewPagerAdapter(
     override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
         val item = imageList[position]
         holder.binding.run {
-            imageContent.loadNotCropImage(item.fileUrl)
+            val requestOptions = RequestOptions().transform(BackgroundColorTransform(Color.WHITE))
+
+            Glide.with(imageContent)
+                .load(item.fileUrl)
+                .apply(requestOptions)
+                .into(imageContent)
+
             imageContent.setOnViewTapListener { view, x, y ->
                 onClickImage()
             }
--- app/src/main/java/com/b1nd/alimo/presentation/utiles/Utiles.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/utiles/Utiles.kt
@@ -1,7 +1,13 @@
 package com.b1nd.alimo.presentation.utiles
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
 import android.view.animation.Animation
+import com.bumptech.glide.load.Key
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+import java.security.MessageDigest
 import java.text.DecimalFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -39,5 +45,30 @@ fun convertBytes(size: Int): String {
         size < gb -> "${decimalFormat.format(size / mb)}MB"
         size < tb -> "${decimalFormat.format(size / gb)}GB"
         else -> "${decimalFormat.format(size / tb)}TB"
+    }
+}
+
+class BackgroundColorTransform(private val backgroundColor: Int) : BitmapTransformation() {
+    override fun transform(
+        pool: BitmapPool,
+        toTransform: Bitmap,
+        outWidth: Int,
+        outHeight: Int
+    ): Bitmap {
+        val width = toTransform.width
+        val height = toTransform.height
+
+        // 새 Bitmap을 생성하고 캔버스에 원하는 배경색을 적용합니다.
+        val bitmap = pool.get(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        canvas.drawColor(backgroundColor)
+
+        // 원본 이미지를 새로운 Bitmap 위에 그립니다.
+        canvas.drawBitmap(toTransform, 0f, 0f, null)
+        return bitmap
+    }
+
+    override fun updateDiskCacheKey(messageDigest: MessageDigest) {
+        messageDigest.update(("BackgroundColorTransform:$backgroundColor").toByteArray(Key.CHARSET))
     }
 }
