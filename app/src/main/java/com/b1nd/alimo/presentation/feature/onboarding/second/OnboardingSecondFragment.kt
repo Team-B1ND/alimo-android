f40cdb0c270e683c43daa21a539809a3b41151a7
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-03-19 19:47:30 +0900

    refactor: RefreshToken Expiration
    세션 만료 한번만 뜨게 수정

--- app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/second/OnboardingSecondFragment.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/onboarding/second/OnboardingSecondFragment.kt
@@ -13,6 +13,7 @@ import com.b1nd.alimo.R
 import com.b1nd.alimo.databinding.FragmentOnboardingSecondBinding
 import com.b1nd.alimo.presentation.base.BaseFragment
 import com.b1nd.alimo.presentation.feature.onboarding.second.OnboardingSecondViewModel.Companion.ON_CLICK_START
+import com.b1nd.alimo.presentation.utiles.collectStateFlow
 import com.b1nd.alimo.presentation.utiles.onSuccessEvent
 import com.b1nd.alimo.presentation.utiles.shortToast
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,11 +28,15 @@ class OnboardingSecondFragment :
 
 
     override fun initView() {
+        viewModel.tokenCheck()
         // RefreshToken 만료됐다면 SnackBar Show
-        Log.d("TAG", "initView: ${args.token}")
-        if (args.token == "만료") {
-            requireContext().shortToast("세션이 만료되었습니다")
-//            viewModel.tokenReset()
+
+        collectStateFlow(viewModel.tokenState){
+            if (it.token == "만료") {
+                Log.d("TAG", "initView: $it")
+                requireContext().shortToast("세션이 만료되었습니다")
+                viewModel.tokenReset()
+            }
         }
 
 
