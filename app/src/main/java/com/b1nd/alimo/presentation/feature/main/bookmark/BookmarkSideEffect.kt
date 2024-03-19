9d1bf34b5a5e3dc567b40a43ef0247c7f0db70f3
8954sood <58807462+8954sood@users.noreply.github.com>
2024-03-19 21:35:29 +0900

    refactor: Edit Object To Data Object

--- app/src/main/java/com/b1nd/alimo/presentation/feature/main/bookmark/BookmarkSideEffect.kt
+++ app/src/main/java/com/b1nd/alimo/presentation/feature/main/bookmark/BookmarkSideEffect.kt
@@ -2,6 +2,6 @@ package com.b1nd.alimo.presentation.feature.main.bookmark
 
 sealed class BookmarkSideEffect {
     data class NetworkError(val message: String): BookmarkSideEffect()
-    object FailedChangeEmoji: BookmarkSideEffect()
-    object FailedChangeBookmark: BookmarkSideEffect()
+    data object FailedChangeEmoji: BookmarkSideEffect()
+    data object FailedChangeBookmark: BookmarkSideEffect()
 }
