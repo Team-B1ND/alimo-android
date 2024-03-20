834dadeac6f43cf09089145e7c6451ee1e009272
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-03-20 12:45:17 +0900

    refactor: Delete RepositoryModule

--- app/src/main/java/com/b1nd/alimo/di/RepositoryModule.kt
+++ app/src/main/java/com/b1nd/alimo/di/RepositoryModule.kt
@@ -1,11 +1,7 @@
 package com.b1nd.alimo.di
 
 import com.b1nd.alimo.data.remote.service.ExampleService
-import com.b1nd.alimo.data.remote.service.FirebaseTokenService
-import com.b1nd.alimo.data.remote.service.TokenService
 import com.b1nd.alimo.data.repository.ExampleRepository
-import com.b1nd.alimo.data.repository.FirebaseTokenRepository
-import com.b1nd.alimo.data.repository.TokenRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -19,16 +15,6 @@ interface RepositoryModule {
     @Singleton
     @Binds
     fun provideExampleRepository(repository: ExampleRepository): ExampleService
-
-
-    @Singleton
-    @Binds
-    fun provideFirebaseTokenRepository(repository: FirebaseTokenRepository): FirebaseTokenService
-
-    @Singleton
-    @Binds
-    fun provideTokenRepository(repository: TokenRepository): TokenService
-
 
 
 
