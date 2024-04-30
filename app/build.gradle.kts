4c08f15a2ce731ab58ec5a1b30a26b4f33bd56c7
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-04-30 19:45:34 +0900

    build: isMinifyEnabled ture

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
             )
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
