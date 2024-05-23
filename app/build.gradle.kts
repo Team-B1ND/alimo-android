619f6ae3f76e3f5c76a773d4b10bb4f30606baf5
wnsgur1 <133772349+wnsgur1@users.noreply.github.com>
2024-05-23 17:10:03 +0900

    chore: Version Up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.b1nd.alimo"
         minSdk = 28
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.1.0"
 
         buildConfigField("String", "SERVER_URL", "${prperties["SERVER_URL"]}")
 
